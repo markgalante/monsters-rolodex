8b1e56ef7d9a945dd0a3621bf22c18ea3f904c95
markgalante <mark.galante.14@gmail.com>
2020-05-23 14:26:46 +0200

    Updates

---
+++ precache-manifest.1d64c7ee982747d3166d81533157bb9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14bb6dae691e585925a2578ea1aaf136",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b8a1e5ec07d971ac530a",
+    "url": "/monsters-rolodex/static/css/main.3a85d51d.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/monsters-rolodex/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8a1e5ec07d971ac530a",
+    "url": "/monsters-rolodex/static/js/main.87c57eaf.chunk.js"
+  },
+  {
+    "revision": "6062e4a584c29c6e0372",
+    "url": "/monsters-rolodex/static/js/runtime-main.e3d252a9.js"
+  }
+]);
